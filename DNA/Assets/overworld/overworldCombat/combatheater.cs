9d0043739e90dcf3de7361d0289c3c1f4e23dc53
Liam Hall <161538967+chlorine7oxide@users.noreply.github.com>
2024-12-06 17:14:44 +1300

    some prites

--- DNA/Assets/overworld/overworldCombat/combatheater.cs
+++ DNA/Assets/overworld/overworldCombat/combatheater.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class combatheater : MonoBehaviour
 {
     public bool moved = false;
+    public int pos = 0;
 
     private void Update()
     {
@@ -20,10 +21,12 @@ public class combatheater : MonoBehaviour
     public IEnumerator move(Vector3 target, float time)
     {
         this.gameObject.GetComponent<Rigidbody2D>().linearVelocity = (target - this.gameObject.transform.position) / time;
+        this.gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
         yield return new WaitForSeconds(time);
         this.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
         this.gameObject.AddComponent<PolygonCollider2D>();
         this.gameObject.GetComponent<PolygonCollider2D>().isTrigger = true;
+        pos = 1;
 
     }
 }
