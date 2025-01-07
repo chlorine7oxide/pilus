1460776cdeee977c79c8cb43a0031d13064b5cd9
Liam Hall <161538967+chlorine7oxide@users.noreply.github.com>
2025-01-07 17:39:28 +1300

    mostly reworking the story

--- DNA/Assets/generalSprites/walkingAnim/mc2/follower.cs
+++ DNA/Assets/generalSprites/walkingAnim/mc2/follower.cs
@@ -7,27 +7,157 @@ public class follower : MonoBehaviour
     public Vector3 entry;
     public GameObject mc;
 
-    public static IEnumerator create(GameObject mc, Vector3 startPos)
+    public static void create(GameObject mc, Vector3 startPos)
     {
-        yield return new WaitUntil(() => (mc.transform.position - startPos).magnitude > 1);
-        GameObject g = new();
+        GameObject g = Instantiate(followerCreator.followerPrefab);
         follower f = g.AddComponent<follower>();
         f.mc = mc;
         f.entry = startPos;
+        g.GetComponent<SpriteRenderer>().sprite = null;
     }
 
-    public Vector2[] positions = new Vector2[50];
+    public Vector3[] positions = new Vector3[15];
     public int posIndex = 0;
+
+    public float animCD = 0;
+    public float animNum = 0;
+
+    public bool startAnim = false;
 
     private void FixedUpdate()
     {
         if (positions[posIndex] != null)
         {
-            // use it placeholder
-        }
+            if (mc.transform.position != positions[(posIndex + 9) % 10])
+            {
+                if (startAnim)
+                {
+                    Vector3 movement = positions[posIndex] - this.gameObject.transform.position;
+                    this.gameObject.transform.position = positions[posIndex];
 
-        positions[posIndex] = mc.transform.position;
-        posIndex++;
-        posIndex %= positions.Length;
+                    if (animCD < 0)
+                    {
+                        if (movement.y > 0 && movement.y > Mathf.Abs(movement.x))
+                        {
+                            //up
+                            this.gameObject.GetComponent<SpriteRenderer>().flipX = false;
+                            switch (animNum)
+                            {
+                                case 0:
+                                    this.gameObject.GetComponent<SpriteRenderer>().sprite = followerCreator.b1;
+                                    break;
+                                case 1:
+                                    this.gameObject.GetComponent<SpriteRenderer>().sprite = followerCreator.b2;
+                                    break;
+                                case 2:
+                                    this.gameObject.GetComponent<SpriteRenderer>().sprite = followerCreator.b3;
+                                    break;
+                                case 3:
+                                    this.gameObject.GetComponent<SpriteRenderer>().sprite = followerCreator.b4;
+                                    break;
+                            }
+                            animNum++;
+                            animNum %= 4;
+
+                        }
+                        else if (movement.y < 0 && Mathf.Abs(movement.y) > Mathf.Abs(movement.x))
+                        {
+                            //down
+                            this.gameObject.GetComponent<SpriteRenderer>().flipX = false;
+                            switch (animNum)
+                            {
+                                case 0:
+                                    this.gameObject.GetComponent<SpriteRenderer>().sprite = followerCreator.f1;
+                                    break;
+                                case 1:
+                                    this.gameObject.GetComponent<SpriteRenderer>().sprite = followerCreator.f2;
+                                    break;
+                                case 2:
+                                    this.gameObject.GetComponent<SpriteRenderer>().sprite = followerCreator.f3;
+                                    break;
+                                case 3:
+                                    this.gameObject.GetComponent<SpriteRenderer>().sprite = followerCreator.f4;
+                                    break;
+                            }
+                            animNum++;
+                            animNum %= 4;
+                        }
+                        else if (movement.x > 0)
+                        {
+                            //right
+                            this.gameObject.GetComponent<SpriteRenderer>().flipX = true;
+                            switch (animNum)
+                            {
+                                case 0:
+                                    this.gameObject.GetComponent<SpriteRenderer>().sprite = followerCreator.s1;
+                                    break;
+                                case 1:
+                                    this.gameObject.GetComponent<SpriteRenderer>().sprite = followerCreator.s2;
+                                    break;
+                                case 2:
+                                    this.gameObject.GetComponent<SpriteRenderer>().sprite = followerCreator.s3;
+                                    break;
+                                case 3:
+                                    this.gameObject.GetComponent<SpriteRenderer>().sprite = followerCreator.s4;
+                                    break;
+                            }
+                            animNum++;
+                            animNum %= 4;
+                        }
+                        else
+                        {
+                            //left
+                            this.gameObject.GetComponent<SpriteRenderer>().flipX = false;
+                            switch (animNum)
+                            {
+                                case 0:
+                                    this.gameObject.GetComponent<SpriteRenderer>().sprite = followerCreator.s1;
+                                    break;
+                                case 1:
+                                    this.gameObject.GetComponent<SpriteRenderer>().sprite = followerCreator.s2;
+                                    break;
+                                case 2:
+                                    this.gameObject.GetComponent<SpriteRenderer>().sprite = followerCreator.s3;
+                                    break;
+                                case 3:
+                                    this.gameObject.GetComponent<SpriteRenderer>().sprite = followerCreator.s4;
+                                    break;
+                            }
+                            animNum++;
+                            animNum %= 4;
+
+                        }
+                        animCD = 0.15f;
+                    }
+                }
+
+                animCD -= Time.fixedDeltaTime;
+
+                
+
+                positions[posIndex] = mc.transform.position;
+                posIndex++;
+                posIndex %= positions.Length;
+
+                if (posIndex == 14)
+                {
+                    startAnim = true;
+                }
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (this.gameObject.transform.position.y > mc.transform.position.y)
+        {
+            mc.GetComponent<SpriteRenderer>().sortingOrder = 8;
+            this.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 7;
+        }
+        else
+        {
+            mc.GetComponent<SpriteRenderer>().sortingOrder = 7;
+            this.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 8;
+        }
     }
 }
--- DNA/Assets/generalSprites/walkingAnim/mc2/followerCreator.cs
+++ DNA/Assets/generalSprites/walkingAnim/mc2/followerCreator.cs
@@ -2,11 +2,44 @@ using UnityEngine;
 
 public class followerCreator : MonoBehaviour
 {
-    public Sprite f1, f2, f3, f4, s1, s2, s3, s4, b1, b2, b3, b4;
+    public static Sprite f1, f2, f3, f4, s1, s2, s3, s4, b1, b2, b3, b4;
+    public Sprite f1_, f2_, f3_, f4_, s1_, s2_, s3_, s4_, b1_, b2_, b3_, b4_;
+
+    public static GameObject followerPrefab;
+    public GameObject followerPrefab_;
 
     private void Start()
     {
-        StartCoroutine(follower.create(gameObject, transform.position));
+        f1 = f1_;
+        f2 = f2_;
+        f3 = f3_;
+        f4 = f4_;
+        s1 = s1_;
+        s2 = s2_;
+        s3 = s3_;
+        s4 = s4_;
+        b1 = b1_;
+        b2 = b2_;
+        b3 = b3_;
+        b4 = b4_;
+
+        followerPrefab = followerPrefab_;
+
+        startpos = this.gameObject.transform.position;
+
+        
+    }
+
+    public Vector3 startpos;
+    public bool created = false;
+
+    private void Update()
+    {
+        if ((this.gameObject.transform.position - startpos).magnitude > 0.5f && !created)
+        {
+            follower.create(gameObject, transform.position);
+            created = true;
+        }
     }
 }
 
